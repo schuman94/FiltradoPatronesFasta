d32b284d403e93f828c9c4796c840cf632382382
schuman94 <sergio.chulian@iesdonana.org>
2023-03-24 13:24:55 +0100

    añadido filtrado alfa 4,7

--- filters.py
+++ filters.py
@@ -3,16 +3,24 @@ import re
 def get_filter_function(filter_option):
     if filter_option == "cuatro_cisteinas":
         return filter_four_cysteines
-    elif filter_option == "patron_CCXXXXCXXXXC":
-        return filter_pattern_CCXXXXCXXXXC
+    elif filter_option == "patron_alfa44":
+        return filter_pattern_alfa44
+    elif filter_option == "patron_alfa47":
+        return filter_pattern_alfa47
     else:
         raise ValueError(f"Opción de filtro desconocida: {filter_option}")
 
 def filter_four_cysteines(record):
     return record.seq.count("C") == 4
 
-def filter_pattern_CCXXXXCXXXXC(record):
+def filter_pattern_alfa44(record):
     pattern = re.compile("CC[^C]{4}C[^C]{4}C")
+    sequence = str(record.seq)
+    matches = pattern.findall(sequence)
+    return len(matches) == 1 and record.seq.count("C") == 4
+
+def filter_pattern_alfa47(record):
+    pattern = re.compile("CC[^C]{4}C[^C]{7}C")
     sequence = str(record.seq)
     matches = pattern.findall(sequence)
     return len(matches) == 1 and record.seq.count("C") == 4
--- main.py
+++ main.py
@@ -11,8 +11,18 @@ def filtrar_secuencias():
         messagebox.showerror("Error", "Selecciona un archivo de entrada y una carpeta de salida")
         return
 
-    file_utils.filtrar_secuencias(input_file, output_folder, filter_option)
-    messagebox.showinfo("Éxito", "El filtrado de secuencias se ha completado")
+    def run_filtering():
+        nonlocal progress_window
+        file_utils.filtrar_secuencias(input_file, output_folder, filter_option)
+        progress_window.destroy()
+        messagebox.showinfo("Éxito", "El filtrado de secuencias se ha completado")
+
+    progress_window = tk.Toplevel(ventana)
+    progress_window.title("Filtrando secuencias")
+    progress_window.geometry("250x100")
+    tk.Label(progress_window, text="Filtrando secuencias, por favor espera...").pack(pady=10)
+    ventana.after(100, run_filtering)  # Ejecuta el filtrado después de 100ms para dar tiempo a que se muestre la ventana
+
 
 # Interfaz gráfica
 ventana = tk.Tk()
@@ -32,9 +42,7 @@ tk.Entry(ventana, textvariable=salida_var, width=60).grid(row=1, column=1)
 tk.Button(ventana, text="Seleccionar", command=file_utils.seleccionar_carpeta(salida_var)).grid(row=1, column=2)
 
 tk.Label(ventana, text="Patrón de filtrado:").grid(row=2, column=0, sticky="e")
-tk.OptionMenu(ventana, patron_var, "cuatro_cisteinas").grid(row=2, column=1, sticky="w")
-patron_var.set("cuatro_cisteinas")
-tk.OptionMenu(ventana, patron_var, "cuatro_cisteinas", "patron_CCXXXXCXXXXC").grid(row=2, column=1, sticky="w")
+tk.OptionMenu(ventana, patron_var, "cuatro_cisteinas", "patron_alfa44", "patron_alfa47").grid(row=2, column=1, sticky="w")
 patron_var.set("cuatro_cisteinas")
 
 tk.Button(ventana, text="Iniciar filtrado", command=filtrar_secuencias).grid(row=3, column=1, pady=10)
